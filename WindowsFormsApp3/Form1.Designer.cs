d14982780b2c18cb46946f85b1e57b8ebfbf42a2
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-04-03 00:10:24 +0700

    init

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.edNaim = new System.Windows.Forms.NumericUpDown();
+            this.edZC = new System.Windows.Forms.NumericUpDown();
+            this.butStart = new System.Windows.Forms.Button();
+            this.labPers = new System.Windows.Forms.Label();
+            this.labPrib = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.edPost = new System.Windows.Forms.NumericUpDown();
+            this.labTime = new System.Windows.Forms.Label();
+            this.labStore = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.edNaim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edZC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edPost)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Найм";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Закупочная цена";
+            // 
+            // edNaim
+            // 
+            this.edNaim.Location = new System.Drawing.Point(218, 45);
+            this.edNaim.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.edNaim.Name = "edNaim";
+            this.edNaim.Size = new System.Drawing.Size(120, 22);
+            this.edNaim.TabIndex = 2;
+            this.edNaim.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // edZC
+            // 
+            this.edZC.Location = new System.Drawing.Point(218, 81);
+            this.edZC.Name = "edZC";
+            this.edZC.Size = new System.Drawing.Size(120, 22);
+            this.edZC.TabIndex = 3;
+            this.edZC.Value = new decimal(new int[] {
+            55,
+            0,
+            0,
+            0});
+            // 
+            // butStart
+            // 
+            this.butStart.Location = new System.Drawing.Point(307, 214);
+            this.butStart.Name = "butStart";
+            this.butStart.Size = new System.Drawing.Size(75, 23);
+            this.butStart.TabIndex = 4;
+            this.butStart.Text = "Next";
+            this.butStart.UseVisualStyleBackColor = true;
+            this.butStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labPers
+            // 
+            this.labPers.AutoSize = true;
+            this.labPers.Location = new System.Drawing.Point(504, 81);
+            this.labPers.Name = "labPers";
+            this.labPers.Size = new System.Drawing.Size(89, 17);
+            this.labPers.TabIndex = 5;
+            this.labPers.Text = "Персонал: 1";
+            this.labPers.Click += new System.EventHandler(this.labPers_Click);
+            // 
+            // labPrib
+            // 
+            this.labPrib.AutoSize = true;
+            this.labPrib.Location = new System.Drawing.Point(504, 118);
+            this.labPrib.Name = "labPrib";
+            this.labPrib.Size = new System.Drawing.Size(123, 17);
+            this.labPrib.TabIndex = 6;
+            this.labPrib.Text = "Прибыль: 100000";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Поставка";
+            // 
+            // edPost
+            // 
+            this.edPost.Location = new System.Drawing.Point(218, 113);
+            this.edPost.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.edPost.Name = "edPost";
+            this.edPost.Size = new System.Drawing.Size(120, 22);
+            this.edPost.TabIndex = 8;
+            this.edPost.Value = new decimal(new int[] {
+            3000,
+            0,
+            0,
+            0});
+            // 
+            // labTime
+            // 
+            this.labTime.AutoSize = true;
+            this.labTime.Location = new System.Drawing.Point(441, 217);
+            this.labTime.Name = "labTime";
+            this.labTime.Size = new System.Drawing.Size(66, 17);
+            this.labTime.TabIndex = 9;
+            this.labTime.Text = "Месяц: 0";
+            // 
+            // labStore
+            // 
+            this.labStore.AutoSize = true;
+            this.labStore.Location = new System.Drawing.Point(504, 45);
+            this.labStore.Name = "labStore";
+            this.labStore.Size = new System.Drawing.Size(98, 17);
+            this.labStore.TabIndex = 10;
+            this.labStore.Text = "Запасы: 3000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 287);
+            this.Controls.Add(this.labStore);
+            this.Controls.Add(this.labTime);
+            this.Controls.Add(this.edPost);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.labPrib);
+            this.Controls.Add(this.labPers);
+            this.Controls.Add(this.butStart);
+            this.Controls.Add(this.edZC);
+            this.Controls.Add(this.edNaim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.edNaim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edZC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edPost)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown edNaim;
+        private System.Windows.Forms.NumericUpDown edZC;
+        private System.Windows.Forms.Button butStart;
+        private System.Windows.Forms.Label labPers;
+        private System.Windows.Forms.Label labPrib;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown edPost;
+        private System.Windows.Forms.Label labTime;
+        private System.Windows.Forms.Label labStore;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const double marketing = 0.5;   //Эффективность рекламы
+        const int DEL = 6;
+        const int oklad = 20000;        //Оклад на 1 чел.
+
+        int store = 3000;   //Запасы
+        int staff = 1;      //Персонал
+        double UKO = 0;     //Уровень качества обслуживания
+
+        double prib = 1000000;  //Начальный капитал      
+        int RSell = 0;          //Розничная продажа
+        double price = 0;       //Цена товара
+        double demand = 0;      //Спрос
+        double ZP = 0;          //Зарплата на весь персонал
+        int time = 0;
+
+        double s1 = 0, out1 = 0, s2 = 0, out2 = 0, s3 = 0, out3 = 0;
+
+        private void labPers_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time++;
+            Random rand = new Random();
+            double ZC = (int)edZC.Value;    //Закупочная стоимость продукции
+            int naim = (int)edNaim.Value;   //Найм
+            int post = (int)edPost.Value;   //Поставки
+
+            //Доставка товара в магазин
+            s3 = s3 + out2 - out3;
+            out3 = Math.Ceiling(s3 / (DEL / 3));
+            s2 = s2 + out1 - out2;
+            out2 = Math.Ceiling(s2 / (DEL / 3));
+            s1 = s1 + post - out1;
+            out1 = Math.Ceiling(s1 / (DEL / 3));          
+            //Наем и увольнение
+            int fired = rand.Next(0, (staff / 2));      //Уволенные
+            staff = staff + naim - fired;               //Количество персонала
+            //Экономика магазина
+            price = Math.Round(ZC * 2, 2);
+            demand = marketing / (price*0.0005);
+            UKO = (ZP / (staff * 1000)) * staff;
+            //Продажи
+            RSell = (int)(Math.Round(demand) * UKO);
+            if (RSell > store) RSell = store;
+            store = store + (int)out3 - RSell;
+            //Зарплата
+            if (RSell < 200) ZP = oklad * 0.8 * staff;      //Начисление премиальных
+            if (RSell > 500) ZP = oklad * 1.1 * staff;
+            else ZP = oklad * 1 * staff;
+            //Подсчет прибыли
+            double profit = Math.Round(RSell * price,2);    //Выручка
+            double rashods = Math.Round(ZC * post+ZP,2);    //Расходы
+            prib = prib + profit - rashods;                 //Прибыль
+
+            labPers.Text = "Персонал: " + staff;
+            labPrib.Text = "Прибыль: " + prib;
+            labStore.Text = "Запасы: " + store;
+            labTime.Text = "Месяц: " + time;
+        }
+    }
+}
